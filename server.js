a6798f22a7b6b28d7a1f01a85e75cd63727e3aa7
Pushpak Rakesh <pushpak.mca11@gmail.com>
2015-10-02 00:10:48 +0530

    add mongoose

---
+++ server.js
@@ -0,0 +1,81 @@
+/*var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});*/
+
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+
+// Mongoose Schema definition
+    Schema = new mongoose.Schema({
+      id       : String, 
+      title    : String,
+      completed: Boolean
+    }),
+
+    Todo = mongoose.model('TicketLogs', Schema);
+
+/*
+ * I’m sharing my credential here.
+ * Feel free to use it while you’re learning.
+ * After that, create and use your own credential.
+ * Thanks.
+ *
+ * MONGOLAB_URI=mongodb://example:example@ds053312.mongolab.com:53312/todolist
+ * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
+ */
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+app.use(bodyParser.json()) // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
+
+
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
+
+  console.log("websocket connection open")
+
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
