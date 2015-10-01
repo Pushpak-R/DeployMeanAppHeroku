2c369cf49a90414b8e253a2b04acff65d481fa3a
Pushpak Rakesh <pushpak.mca11@gmail.com>
2015-10-01 23:05:03 +0530

    dummy app for deployment testing

--- server.js
+++ server.js
@@ -1,64 +1,23 @@
-// MEAN Stack RESTful API Tutorial - Contact List App
-
 var express = require('express');
 var app = express();
-//var mongojs = require('mongojs');
-//var db = mongojs('ticketlist', ['ticketlist']);
-var bodyParser = require('body-parser');
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
 
+// set the home page route
+app.get('/', function(req, res) {
 
-
-db.ticketlist.remove();
-
-
-
-app.get('/ticketlist', function (req, res) {
-  console.log('I received a GET request');
-
-  db.ticketlist.find(function (err, docs) {
-    console.log(docs);
-    res.json(docs);
-  });
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-
-app.post('/ticketlist', function (req, res) {
-  console.log(req.body);
-  db.ticketlist.insert(req.body, function(err, doc) {
-    res.json(doc);
-  });
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
-
-app.delete('/ticketlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.ticketlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-
-app.get('/ticketlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(id);
-  db.ticketlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
-    res.json(doc);
-  });
-});
-
-app.put('/ticketlist/:id', function (req, res) {
-  var id = req.params.id;
-  console.log(req.body.name);
-  db.ticketlist.findAndModify({
-    query: {_id: mongojs.ObjectId(id)},
-    update: {$set: {c_info: req.body.c_info, comments: req.body.comments, createdBy: req.body.createdBy, assignedTo: req.body.assignedTo, status: req.body.status}},
-    new: true}, function (err, doc) {
-      res.json(doc);
-    }
-  );
-});
-var port = process.env.PORT || 5000
-app.listen(port);
-console.log("Server running on port 3000");
